366d8fa4c8230d292b57e87a68f4040f1a4336d9
mariaam123 <mariam.bakradze.1@btu.edu.ge>
2023-08-01 01:59:15 +0400

    edited

--- src/main/java/lang/print/gaps/task5/NumberDivider.java
+++ src/main/java/lang/print/gaps/task5/NumberDivider.java
@@ -2,8 +2,8 @@ package lang.print.gaps.task5;
 
 public class NumberDivider {
     void divide(int toBeDivided) {
-        int result = toBeDivided / 5;
-        System.out.println("Result: " + result);
+        float result = toBeDivided / 5;
+        System.out.println(result);
     }
 
     public static void main(String[] args) {
